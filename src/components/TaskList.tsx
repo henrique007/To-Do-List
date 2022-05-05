d0ff60038eebc73f8d5bc8595eaa3f73cb4bc086
Dayvson Henrique Santos Timóteo <dayvsonn.ti@outlook.com>
2022-05-05 09:43:45 -0300

    desafio concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    
+    const newTask = {
+      id: Math.floor(Date.now() * Math.random()),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldState => [...oldState, newTask])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasksMap = tasks.map((task) => {
+      if (task.id === id) {
+        task.isComplete = true
+        return task
+      } else{
+        return task
+      }
+    })
+    setTasks(newTasksMap)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasksFilter = tasks.filter(task => task.id !== id)
+    setTasks(newTasksFilter)
   }
 
   return (
